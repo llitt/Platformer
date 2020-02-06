4d308d254311cee27a68c25560fa415773652d21
llitt <56178356+llitt@users.noreply.github.com>
2020-02-05 19:31:30 -0800

    Demo: Added Season Filter

---
+++ Assets/FadeIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+   Material filter;
+   Color targetColor;
+   public Color[] seasonfilters;
+   float maxalpha;
+    // Start is called before the first frame update
+    void Start()
+    {
+      filter = GetComponent<MeshRenderer>().material;
+      maxalpha = filter.color.a;
+      filter.color = new Color(filter.color.r, filter.color.g, filter.color.b, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      filter.color=Color.Lerp(filter.color, targetColor, .05f);
+      targetColor = seasonfilters[LevelManager.LM.timestage];
+      
+   }
+}
