fbf10304a1db8f1defae0003f84b228f446086dc
llitt <56178356+llitt@users.noreply.github.com>
2020-02-19 18:35:09 -0800

    Better Prefab

--- Assets/Scripts/Camera/CameraMoveCheckpoint.cs
+++ Assets/Scripts/Camera/CameraMoveCheckpoint.cs
@@ -10,7 +10,7 @@ public class CameraMoveCheckpoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      GetComponent<MeshRenderer>().enabled = false;
     }
 
     // Update is called once per frame
