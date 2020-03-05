11330a65ad3c7d784e1449579071e4422c405227
Eli P <elijahpinson@gmail.com>
2020-03-04 19:19:36 -0800

    Cube Pusher minion added to level 0

---
+++ Assets/Scripts/NPC/Enemies/Minions/CubePusher/CubePusher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePusher : MonoBehaviour
+{
+    private Vector3 target;
+    private float speed = 18;
+    private Rigidbody myRigidBody;
+    private Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = gameObject.transform.position;
+        target = startPos;
+        myRigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.z < target.z)
+        {
+            //Debug.Log("moving left");
+            myRigidBody.AddForce(Vector3.forward * speed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        else if (gameObject.transform.position.z > target.z)
+        {
+            //Debug.Log("moving right");
+            myRigidBody.AddForce(-Vector3.forward * speed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+
+    public void setTarget(Vector3 goTo)
+    {
+        target = goTo;
+    }
+
+    public void resetPosition()
+    {
+        target = startPos;
+        //Debug.Log("resetting cube pusher's position");
+    }
+}
---
+++ Assets/Scripts/NPC/Enemies/Minions/CubePusher/DetectionArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionArea : MonoBehaviour
+{
+    public CubePusher cPusher;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            cPusher.setTarget(other.transform.position);
+            //Debug.Log("target set to " + other.transform.position);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("target left detection area");
+        cPusher.resetPosition();
+    }
+}
