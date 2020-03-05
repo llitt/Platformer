39c5b8a6cfcd5b4981225bc5210c2a4f45e370a2
Eli P <elijahpinson@gmail.com>
2020-03-04 19:40:41 -0800

    Cave minion and ice platform
    Added minion and ice on first platform in cave

--- Assets/Scripts/Environment/Seasons/SelectWheel.cs
+++ Assets/Scripts/Environment/Seasons/SelectWheel.cs
@@ -21,7 +21,7 @@ public class SelectWheel : MonoBehaviour
  
       float ymov = Input.GetAxis("Mouse Y");
       //Vector3 offset=new Vector3 (0, ymov, xmov);
-      Debug.Log(Input.mousePosition);
+      //Debug.Log(Input.mousePosition);
       //transform.LookAt(new Vector3(0,Input.mousePosition.y, Input.mousePosition.x));
       transform.LookAt(mouse);
       transform.localRotation.SetEulerRotation(transform.localRotation.eulerAngles.x,0,transform.localRotation.eulerAngles.z);
