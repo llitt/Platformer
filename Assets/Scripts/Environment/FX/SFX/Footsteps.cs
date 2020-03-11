2e9573bcee4be3b98878812e2b9df4cefab4bf10
llitt <56178356+llitt@users.noreply.github.com>
2020-03-11 15:04:04 -0700

    Update Footsteps.cs

--- Assets/Scripts/Environment/FX/SFX/Footsteps.cs
+++ Assets/Scripts/Environment/FX/SFX/Footsteps.cs
@@ -4,24 +4,42 @@ using UnityEngine;
 
 public class Footsteps : MonoBehaviour
 {
-    public Player chara;
-    public AudioClip[] snowClips;
-    public AudioClip[] sandClips;
-    public AudioClip[] grassClips;
-    public AudioClip[] swampClips;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public Player chara;
+   public AudioClip[] snowClips;
+   public AudioClip[] sandClips;
+   public AudioClip[] grassClips;
+   public AudioClip[] swampClips;
+   // Start is called before the first frame update
+   void Start()
+   {
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (chara.grounded && (Input.GetAxis("Horizontal") != 0) && gameObject.GetComponent<AudioSource>().isPlaying == false)
-        {
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (chara.grounded && (Input.GetAxis("Horizontal") != 0) && gameObject.GetComponent<AudioSource>().isPlaying == false)
+      {
+         if (LevelManager.LM.timestage == 2)
+         {
             GetComponent<AudioSource>().clip = snowClips[Random.Range(0, snowClips.Length)];
             GetComponent<AudioSource>().Play();
-        }
-    }
+         }
+         else if (LevelManager.LM.timestage == 0)
+         {
+            GetComponent<AudioSource>().clip = grassClips[Random.Range(0, grassClips.Length)];
+            GetComponent<AudioSource>().Play();
+         }
+         else if (LevelManager.LM.timestage == 1)
+         {
+            GetComponent<AudioSource>().clip = sandClips[Random.Range(0, sandClips.Length)];
+            GetComponent<AudioSource>().Play();
+         }
+         else if (LevelManager.LM.timestage == 3)
+         {
+            GetComponent<AudioSource>().clip = swampClips[Random.Range(0, swampClips.Length)];
+            GetComponent<AudioSource>().Play();
+         }
+      }
+   }
 }
