5d43fa6587d1154b4780b8f7e4ebaad56c518536
llitt <56178356+llitt@users.noreply.github.com>
2020-03-10 17:23:44 -0700

    added a script the snow layer changes color to orange/brown/green

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+   public Color[] colors;
+   public int lastseason;
+    // Start is called before the first frame update
+    void Start()
+    {
+      lastseason = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (LevelManager.LM.timestage != lastseason) {
+         SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
+         foreach (SpriteRenderer sprite in sprites) {
+            sprite.color = colors[LevelManager.LM.timestage];
+         }
+         lastseason = LevelManager.LM.timestage;
+      }
+    }
+}
