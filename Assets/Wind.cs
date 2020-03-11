b48d9f4080a833501fa8917d0c9c0cae5940e9ce
llitt <56178356+llitt@users.noreply.github.com>
2020-03-10 22:26:58 -0700

    BossLevel4 done

---
+++ Assets/Wind.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+   float timer;
+   public float timebeforeffect = .2f,forcemult=35;
+   public Vector3 direction=Vector3.up;
+   // Start is called before the first frame update
+   private void OnTriggerStay(Collider other)
+   {
+      timer += Time.deltaTime;
+      if (timer>timebeforeffect)
+         other.gameObject.GetComponent<Rigidbody>().AddRelativeForce(direction*forcemult, ForceMode.Acceleration);
+   }
+   private void OnTriggerExit(Collider other)
+   {
+      timer = 0;
+   }
+}
