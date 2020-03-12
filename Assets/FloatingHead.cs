e532e60d468cb43981ad895a41e31394449ede47
llitt <56178356+llitt@users.noreply.github.com>
2020-03-12 00:59:27 -0700

    More

---
+++ Assets/FloatingHead.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingHead : MonoBehaviour
+{
+   public GameObject plant;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      GetComponent<LineRenderer>().SetPosition(1, plant.transform.position); 
+    }
+}
