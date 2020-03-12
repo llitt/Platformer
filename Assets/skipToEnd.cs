8486f939e80305e1ce76ccf6dab992b9fe29e781
unknown <mwuchen@calpoly.edu>
2020-03-12 07:10:33 -0700

    little more

---
+++ Assets/skipToEnd.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skipToEnd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Player"){
+            collision.collider.transform.position = new Vector3(0, 14.54f, 580f);
+        }
+    }
+}
