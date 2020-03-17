ba4b9a3353613dbf15ea74ae32c89afe59a5b6a7
llitt <56178356+llitt@users.noreply.github.com>
2020-03-17 14:04:49 -0700

    Worked in desert level

---
+++ Assets/L2_2Plate.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L2_2Plate : MonoBehaviour
+{
+   private Vector3 startingpos;
+   public GameObject cam,cutscenecam;
+   public GameObject pl;
+   public float timer = 0f;
+   private bool down = false;
+   void Start()
+   {
+      startingpos = transform.position;
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (down == false)
+      {
+         transform.position = startingpos;
+
+      }
+      else
+      {
+         L2_2Pyramid.cutscene = true;
+         if (timer < 4)
+         {
+            pl.SetActive(false); ;
+            cam.SetActive(false);
+            cutscenecam.SetActive(true);
+            timer += Time.deltaTime;
+         }
+         else
+         {
+            pl.SetActive(true); 
+            cam.SetActive(true);
+            cutscenecam.SetActive(false);
+         }
+      }
+   }
+   private void OnCollisionEnter(Collision collision)
+   {
+      if ((collision.gameObject.tag == "Player" || collision.gameObject.tag == "Movable_Object"))
+      {
+         down = true;
+      }
+   }
+   private void OnCollisionExit(Collision collision)
+   {
+      if ((collision.gameObject.tag == "Player" || collision.gameObject.tag == "Movable_Object"))
+      {
+         down = false;
+      }
+   }
+
+}
---
+++ Assets/L2_2Pyramid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L2_2Pyramid : MonoBehaviour
+{
+    public static bool cutscene=false;
+   private Vector3 currentLoc;
+   public Transform refernce;
+    void Start()
+    {
+      currentLoc = transform.position;
+      transform.position = new Vector3(transform.position.x, refernce.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (cutscene) {
+         transform.position = Vector3.Lerp(transform.position, currentLoc, 1 * Time.deltaTime);
+      }
+    }
+}
