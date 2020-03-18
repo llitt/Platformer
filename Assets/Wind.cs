d766f75a094503e930b49287f98ba49a5d0a24fe
llitt <56178356+llitt@users.noreply.github.com>
2020-03-17 21:36:55 -0700

    Updated Wind

--- Assets/Wind.cs
+++ Assets/Wind.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class Wind : MonoBehaviour
 {
    float timer;
+   bool fly=false;
    public float timebeforeffect = .2f;
    public Vector3 direction=Vector3.up;
    // Start is called before the first frame update
+   private void Update()
+   {
+      if (fly == true)
+      {
+         timer += Time.deltaTime;
+      }
+   }
+   private void FixedUpdate()
+   {
+      if (timer > timebeforeffect)
+      {
+         Player.player.GetComponent<Rigidbody>().velocity += direction * Time.deltaTime;
+      }
+   }
    private void OnTriggerStay(Collider other)
    {
-      timer += Time.deltaTime;
-      if (timer>timebeforeffect)
-         other.gameObject.GetComponent<Rigidbody>().velocity= other.gameObject.GetComponent<Rigidbody>().velocity+direction*Time.deltaTime;
+      fly = true;
    }
    private void OnTriggerExit(Collider other)
    {
+      fly = false;
       timer = 0;
    }
 }
